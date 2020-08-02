96b652f08d46818c89d6cd02d376d905ac4b5398
Matthijs Boet <m.boet@chello.nl>
2020-08-03 01:22:50 +0200

    finished react-icons

--- practical-react/icons/src/App.js
+++ practical-react/icons/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { IconContext } from 'react-icons'
+import { FaReact } from 'react-icons/fa'
+import { MdSentimentSatisfied } from "react-icons/md";
+import { AiFillAndroid } from 'react-icons/ai'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IconContext.Provider value={{ color: "grey", size: "5rem" }}>
+        <div>
+          <FaReact />
+          <MdSentimentSatisfied />
+          <AiFillAndroid />
+        </div>
+      </IconContext.Provider>
+
+      <FaReact color="#324d67" size="10rem" />
+      <MdSentimentSatisfied color="red" size="10rem" />
+      <AiFillAndroid color="green" size="10rem" />
     </div>
   );
 }
