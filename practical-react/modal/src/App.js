c10ce1e2e03e006d6544b3fa1415e56071a30f4b
Matthijs Boet <m.boet@chello.nl>
2020-08-03 02:18:25 +0200

    finished modals

--- practical-react/modal/src/App.js
+++ practical-react/modal/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import Modal from "react-modal";
+import "./App.css";
 
-import './App.css';
+// Needed for accessibility reasons
+Modal.setAppElement("#root");
 
 function App() {
+  const [modalisOpen, setModalisOpen] = useState(false);
   return (
     <header className="App">
-
+      <button onClick={() => setModalisOpen(true)}>Open Modal</button>
+      <Modal
+        isOpen={modalisOpen}
+        onRequestClose={() => setModalisOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: 'lightgrey'
+          },
+          content: {
+            color: 'orange',
+            borderRadius: '25px',
+            margin: '20vw',
+            paddingLeft: '40px'
+          }
+        }}
+      >
+        <h2>Modal Title</h2>
+        <p>Modal body</p>
+        <div>
+          <button
+            onClick={() => setModalisOpen(false)}
+          >Close</button>
+        </div>
+      </Modal>
     </header>
   );
 }
