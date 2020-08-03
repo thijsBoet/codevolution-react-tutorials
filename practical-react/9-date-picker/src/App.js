08b9dee0fa60176e6cf68538d9314dfc3c297361
Matthijs Boet <m.boet@chello.nl>
2020-08-04 01:55:33 +0200

    finished date-picker

--- practical-react/9-date-picker/src/App.js
+++ practical-react/9-date-picker/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import DatePicker from 'react-datepicker';
 
 import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 className="heading">Select Date</h1>
+        <DatePicker
+          className="datepicker"
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          dateFormat="dd/MM/yyyy"
+          // previous dates disabled
+          minDate={new Date()}
+
+          // future dates disabled
+          // maxDate={new Date()}
+
+          // no weekend dates
+          filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+
+          // clear button
+          isClearable={true}
+
+          showYearDropdown
+
+          scrollableMonthYearDropdown
+        />
       </header>
     </div>
   );
