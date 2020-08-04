d579658bf1010889d4988337f4e6f12737eeb50d
Matthijs Boet <m.boet@chello.nl>
2020-08-04 14:29:57 +0200

    finished practical react series

--- practical-react/12-loading-indicators/src/App.js
+++ practical-react/12-loading-indicators/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import { css } from '@emotion/core';
+import { BounceLoader, BarLoader, BeatLoader } from 'react-spinners'
 
 import './App.css';
+
+const loaderCSS = css`
+  margin-top: 25px;
+  margin-bottom: 25px;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <BounceLoader css={loaderCSS} loading size={24} color='red'/>
+        <BarLoader css={loaderCSS} loading size={48} color='orange'/>
+        <BeatLoader css={loaderCSS} loading size={72} color='maroon'/>
     </div>
   );
 }
